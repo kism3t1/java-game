0039256a46ae9b763e5af49b367301babc98e344
LosOjos <sicjoshsic@gmail.com>
2012-05-29 11:59:31 +0100

    HUD layout improvements: Armour bar only shows when you have armour, is smaller than health bar and now positioned above health bar.

--- src/HUD.java
+++ src/HUD.java
@@ -19,13 +19,13 @@ public class HUD extends Halja{
 			g.drawImage(HUDIcons[HUD_HEART], centreOffset1 + ((i - 1) * HUDIcons[HUD_HEART].getWidth()), height, null);
 		}
 		*/
-		Rectangle rectHealth = new Rectangle((guiWidth / 2) - 100, guiHeight - 50, 200, 20);
+		Rectangle rectHealth = new Rectangle((guiWidth / 2) - 100, guiHeight - 25, 200, 20);
 		g.setColor(Color.BLACK);
 		g.fillRect(rectHealth.x, rectHealth.y, rectHealth.width, rectHealth.height);
 		
 		g.setColor(Color.RED);
 		pcntHealth = (float)world.ollie.getHealth() / (float)world.ollie.getMaxHealth();
-		g.fillRect(rectHealth.x + 1, rectHealth.y + 1, (int) ((rectHealth.width - 2) * pcntHealth), rectHealth.height - 1);
+		g.fillRect(rectHealth.x + 1, rectHealth.y + 1, (int) ((rectHealth.width - 2) * pcntHealth), rectHealth.height - 2);
 		
 		
 		//draw entity armour
@@ -37,13 +37,15 @@ public class HUD extends Halja{
 			g.drawImage(HUDIcons[HUD_SWORD], centreOffset2 + ((i - 1) * HUDIcons[HUD_SWORD].getWidth()), height, null);
 		}
 		*/
-		Rectangle rectArmour = new Rectangle((guiWidth / 2) - 100, guiHeight - 25, 200, 20);
-		g.setColor(Color.BLACK);
-		g.fillRect(rectArmour.x, rectArmour.y, rectArmour.width, rectArmour.height);
-		
-		g.setColor(Color.LIGHT_GRAY);
-		pcntArmour = (float)world.ollie.getArmour() / (float)world.ollie.getMaxArmour();
-		g.fillRect(rectArmour.x + 1, rectArmour.y + 1, (int) ((rectArmour.width - 2) * pcntArmour), rectArmour.height - 1);
+		if(world.ollie.getArmour() > 0){
+			Rectangle rectArmour = new Rectangle((guiWidth / 2) - 100, guiHeight - 40, 200, 10);
+			g.setColor(Color.BLACK);
+			g.fillRect(rectArmour.x, rectArmour.y, rectArmour.width, rectArmour.height);
+
+			g.setColor(Color.LIGHT_GRAY);
+			pcntArmour = (float)world.ollie.getArmour() / (float)world.ollie.getMaxArmour();
+			g.fillRect(rectArmour.x + 1, rectArmour.y + 1, (int) ((rectArmour.width - 2) * pcntArmour), rectArmour.height - 2);
+		}
 	}
 
 }
